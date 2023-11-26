5224a994ed9b12d74d243b71f9d9fdc2593bec0e
Pawan123Pawan <pawanmaurya4277@gmail.com>
2023-11-26 17:22:34 +0530

    pushcode

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import SignUp from "./pages/signUp";
+import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 
 function App() {
